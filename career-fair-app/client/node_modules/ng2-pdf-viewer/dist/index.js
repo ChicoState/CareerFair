37e7dd0ddc52a33b59d0cb2c42779252dd5a1406
cmartinez77 <cmartinez77@mail.csuchico.edu>
2017-05-06 23:50:47 -0700

    untracked node_modules folder

--- career-fair-app/client/node_modules/ng2-pdf-viewer/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pdf_viewer_component_1 = require("./pdf-viewer.component");
-exports.PdfViewerComponent = pdf_viewer_component_1.PdfViewerComponent;
-//# sourceMappingURL=index.js.map
